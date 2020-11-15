8a97c839797facb33b0b6642070c2aadaf72225c
jecis8 <71449002+jecis8@users.noreply.github.com>
2020-11-15 23:30:37 +0200

    Add files via upload

---
+++ 40000000000.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main( ) {
+
+
+   long long i;
+        long long j;
+
+   printf( "Ievadiet pirmo skaitli :");
+   scanf("%lli", &i);
+   printf( "Ievadiet otro skaitli :");
+   scanf("%lli", &j);
+   printf( "Rezultāts: %lli", i*j);
+
+   return 0;
+}
